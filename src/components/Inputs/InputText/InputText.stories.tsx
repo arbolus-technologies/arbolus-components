17824e50e071e79875dca60611f03abae5c02629
JosepSalvat <josep.salvat@arbolus.com>
2023-03-14 12:43:35 +0100

    WIP

---
+++ src/components/Inputs/InputText/InputText.stories.tsx
@@ -0,0 +1,87 @@
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+import { InputText, InputTextProps } from './InputText';
+
+export default {
+  component: InputText,
+  title: 'InputText',
+  argTypes: {
+        defaultValue: {
+            name: 'defaultValue',
+            type: { name: 'string', required: false },
+        },
+        placeholder: {
+            name: 'placeholder',
+            type: { name: 'string', required: false },
+            table: {
+                type: 'string',
+                defaultValue: { summary: '' },
+            },
+        },
+        required: {
+            name: 'required',
+            type: { name: 'boolean', required: false },
+            table: {
+                defaultValue: { summary: 'false' },
+              },
+              control: {
+                type: 'boolean'
+              }
+        },
+        disabled: {
+            name: 'disabled',
+            type: { name: 'boolean', required: false },
+            table: {
+                defaultValue: { summary: 'false' },
+              },
+              control: {
+                type: 'boolean'
+              }
+        },
+        invalid: {
+          name: 'invalid',
+          type: { name: 'boolean', required: false },
+          table: {
+              defaultValue: { summary: 'false' },
+            },
+            control: {
+              type: 'boolean'
+            }
+      },
+        symbol: {
+            name: 'symbol',
+            type: { name: 'string', required: false },
+            table: {
+                type: 'string',
+                defaultValue: { summary: '' },
+            },
+        },
+        // errors: FieldErrors<any>;
+  }
+} as Meta<typeof InputText>;
+
+const Template: Story<InputTextProps> = args => <InputText {...args} />;
+
+export const Primary: Story<InputTextProps> = Template.bind({});
+Primary.args = {
+  placeholder:'Primary input'
+}
+  
+
+export const Error: Story<InputTextProps> = Template.bind({});
+Error.args = {
+  invalid: true,
+  
+};
+
+export const Required: Story<InputTextProps> = Template.bind({});
+Required.args = {
+  required: true,
+};
+
+export const Disabled: Story<InputTextProps> = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+
---
+++ src/components/Inputs/InputText/InputText.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import classNames from 'classnames';
+import React from 'react';
+import { FieldErrors } from 'react-hook-form';
+import { FormFeedback, Input, InputGroup, InputGroupText } from 'reactstrap';
+import styles from './InputText.module.scss';
+import { Disabled } from './InputText.stories';
+
+export interface InputTextProps {
+  defaultValue?: string;
+  placeholder: string;
+  required?: boolean;
+  invalid?: boolean;
+  disabled?: boolean;
+  symbol?: string;
+  errorMsg?: string;
+}
+
+export const InputText: React.FC<InputTextProps> = ({
+  placeholder,
+  invalid,
+  defaultValue,
+  disabled,
+  required,
+  symbol,
+  errorMsg
+}): JSX.Element => {
+
+
+  return (
+    <div className={classNames(styles.inputContainer)}>
+          <InputGroup>
+          <Input type="text" required={required} disabled={disabled} invalid={invalid} placeholder={placeholder} defaultValue={defaultValue} />
+          {errorMsg && (<FormFeedback>
+            {errorMsg}
+          </FormFeedback>)}
+          {symbol && (<InputGroupText>
+            {symbol}
+          </InputGroupText>)}
+        </InputGroup>
+    </div>
+  );
+};
