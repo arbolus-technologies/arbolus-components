63cea9b345709f8c51f5c943cae81d4b6bd8c91e
JosepSalvat <josep.salvat@arbolus.com>
2023-04-24 14:50:24 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: '@arbolus-components',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
 
   css: {
     preprocessorOptions: {
