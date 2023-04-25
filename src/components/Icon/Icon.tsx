9013c9a85cfe3b00cf758e84b33f5dff91fc3957
JosepSalvat <josep.salvat@arbolus.com>
2023-04-25 09:24:21 +0200

    feat(icon): add Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,44 @@
+import classNames from 'classnames'
+import React from 'react'
+import styles from './Icon.module.scss'
+
+interface IconProps {
+  iconName: string
+  fontSize: string
+  color?: string
+  filled?: boolean
+  onClick?: () => void
+  customClasses?: string
+  style?: React.CSSProperties
+  id?: string
+  tooltip?: string
+}
+export const Icon: React.FC<IconProps> = ({
+  iconName,
+  color,
+  fontSize,
+  filled = false,
+  onClick,
+  customClasses,
+  style,
+  id,
+  tooltip,
+}) => (
+  <span
+    data-testid='icon'
+    onClick={onClick}
+    style={{
+      ...style,
+      fontSize,
+      color,
+      cursor: onClick ? 'pointer' : 'default',
+    }}
+    className={classNames('material-symbols-sharp icon', customClasses, {
+      [styles.filled]: filled,
+    })}
+    id={id ? id : 'icon'}
+    title={tooltip}
+  >
+    {iconName}
+  </span>
+)
