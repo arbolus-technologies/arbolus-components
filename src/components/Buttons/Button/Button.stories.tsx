397a51475a543fa3ea5e2cfd8c7e113557548a29
dserna <daniel.serna@arbolus.com>
2023-08-17 14:52:44 +0200

    feat(button): Create button component with unit tests

---
+++ src/components/Buttons/Button/Button.stories.tsx
@@ -0,0 +1,100 @@
+import { jest } from '@storybook/jest'
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from './Button'
+
+const meta: Meta<ButtonProps> = {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    type: {
+      options: ['primary', 'secondary', 'tertiary', 'confirmation', 'rejection'],
+      control: { type: 'select' },
+      defaultValue: 'primary',
+      table: {
+        type: { summary: 'select' }
+      }
+    },
+    text: {
+      control: 'text',
+      table: {
+        type: { summary: 'string' }
+      }
+    },
+    onClick: {
+      action: 'clicked',
+      table: {
+        defaultValue: { summary: '() => console.log("click")' },
+        type: { summary: 'function' }
+      }
+    },
+    disabled: {
+      control: 'boolean',
+      defaultValue: false,
+      table: {
+        type: { summary: 'boolean' }
+      }
+    },
+    nativeType: {
+      options: ['submit', 'reset', 'button'],
+      control: { type: 'select' },
+      defaultValue: 'button'
+    },
+    startIcon: {
+      control: 'text',
+      table: {
+        type: { summary: 'string' }
+      }
+    },
+    endIcon: {
+      control: 'text',
+      table: {
+        type: { summary: 'string' }
+      }
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+  args: {
+    text: 'Primary',
+    onClick: jest.fn(),
+    disabled: false,
+    nativeType: 'button',
+    type: 'primary'
+  }
+}
+
+export const Secondary: Story = {
+  args: {
+    ...Primary.args,
+    text: 'Secondary',
+    type: 'secondary'
+  }
+}
+
+export const Tertiary: Story = {
+  args: {
+    ...Primary.args,
+    text: 'Tertiary',
+    type: 'tertiary'
+  }
+}
+
+export const Confirmation: Story = {
+  args: {
+    ...Primary.args,
+    text: 'Confirmation',
+    type: 'confirmation'
+  }
+}
+
+export const Rejection: Story = {
+  args: {
+    ...Primary.args,
+    text: 'Rejection',
+    type: 'rejection'
+  }
+}
