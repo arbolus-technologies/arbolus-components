def5e9485df1c27e08ef2c6298dc7f9ecb3ad924
dserna-arbolus <85174667+dserna-arbolus@users.noreply.github.com>
2023-08-21 15:50:19 +0200

    feat(config): fix configuration and remove modals

--- src/components/Modals/ArbolusModal.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { InternalArbolusModal, InternalArbolusModalProps } from './InternalArbolusModal'
-
-export const ArbolusModal: React.FC<InternalArbolusModalProps> = (props) => (
-  <InternalArbolusModal {...props} />
-)
