e2a1c5a2dc87d67a2bab0b0442614ca3fe23a70a
dserna <daniel.serna@arbolus.com>
2023-08-22 14:10:57 +0200

    Remove unused colors and types

--- src/theme/colors/colorConstants.ts
+++ src/theme/colors/colorConstants.ts
@@ -1,16 +1,3 @@
-export const YELLOW = '#FFE580';
-export const BLUE = '#96BFFF';
-export const PALE_BLUE = '#B1B9FF';
-export const CYAN = '#ADE4F7';
-export const BRIGHT_GREEN = '#94E8A2';
-export const GREEN = '#CDE894';
-export const PURPLE = '#DBA8ED';
-export const PINK = '#F5CDED';
-export const RED = '#FFAD9D';
-export const BEIGE = '#E8C694';
-export const WHITE = '#FFFFFF';
-export const BLACK = '#000000';
-
 export enum ARBOLUS_COLORS {
   bColorBaseDark = '#000A3F',
   bColorBaseOrange = '#FF5A3A',
@@ -35,37 +22,3 @@ export enum ARBOLUS_COLORS {
   angleDefaultBackgroundColor = '#F0F0F0',
   angleDefaultColor = '#BEBEBE'
 }
-
-export type PALETTE =
-  | typeof ARBOLUS_COLORS.bColorBaseDark
-  | typeof ARBOLUS_COLORS.bColorBaseOrange
-  | typeof ARBOLUS_COLORS.bColorBasePurple
-  | typeof ARBOLUS_COLORS.bColorBaseBlue
-  | typeof ARBOLUS_COLORS.bColorBaseWhite
-  | typeof ARBOLUS_COLORS.bColorSecondaryGreyDark
-  | typeof ARBOLUS_COLORS.bColorSecondaryGreyMid
-  | typeof ARBOLUS_COLORS.bColorSecondaryGreyLight
-  | typeof ARBOLUS_COLORS.bColorAccentPurpleDark
-  | typeof ARBOLUS_COLORS.bColorAccentPurpleMid
-  | typeof ARBOLUS_COLORS.bColorAccentPurpleLight
-  | typeof ARBOLUS_COLORS.bColorAccentOrangeDark
-  | typeof ARBOLUS_COLORS.bColorSecondaryAmber
-  | typeof ARBOLUS_COLORS.bColorAccentAmberDark
-  | typeof ARBOLUS_COLORS.bColorSecondaryGreen
-  | typeof ARBOLUS_COLORS.bColorGrayIcon
-  | typeof ARBOLUS_COLORS.bColorAccentGreenDark
-  | typeof ARBOLUS_COLORS.bColorSecondaryGreyLightBackground;
-
-export type COLOR =
-  | typeof YELLOW
-  | typeof BLUE
-  | typeof PALE_BLUE
-  | typeof CYAN
-  | typeof BRIGHT_GREEN
-  | typeof GREEN
-  | typeof PURPLE
-  | typeof PINK
-  | typeof BEIGE
-  | typeof RED
-  | typeof WHITE
-  | typeof BLACK;
