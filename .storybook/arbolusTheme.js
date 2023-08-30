67550228c096e8ea16dae20aedae9c631e68c50c
Maciej Lenartowicz <maciej.lenartowicz@arbolus.com>
2023-08-30 15:56:56 +0200

    chore(deps): update yarn, remove static folder, move packages to dev dependencies

--- .storybook/arbolusTheme.js
+++ .storybook/arbolusTheme.js
@@ -1,10 +1,11 @@
 import { create } from '@storybook/theming';
-import arbolusLogo from './arbolusLogo.png'
 
-export default create({
+import arbolusLogo from './arbolusLogo.png';
+
+export default create( {
   base: 'light',
   brandTitle: 'Arbolus Storybook',
   brandUrl: 'https://www.arbolus.com',
   brandImage: arbolusLogo,
   brandTarget: '_self',
-});
+} );
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,8 +1,9 @@
 // .storybook/manager.js
 
 import { addons } from '@storybook/addons';
+
 import ArbolusTheme from './arbolusTheme';
 
-addons.setConfig({
+addons.setConfig( {
   theme: ArbolusTheme,
-});
+} );
