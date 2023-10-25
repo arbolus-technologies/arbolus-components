3720833fde9852464615f1f4b4869a66dd6d4e5a
Josep Salvat <josep.salvat@arbolus.com>
2023-10-25 13:07:42 +0200

    fix(components): Exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export * from './Buttons/Button/Button';
 export * from './Buttons/IconButton/IconButton';
 export * from './Icon/Icon';
-export * from './ArbolusAvatar/ArbolusAvatar';
 export * from './FeedbackCollector/FeedbackCollector';
+export * from './ArbolusAvatar/ArbolusAvatar';
