9bf882f6abe6ce82cb5e950bc9b2eccd2e6ec88a
Josep Salvat <josep.salvat@arbolus.com>
2023-10-25 12:56:40 +0200

    Fix export components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { Button, ButtonProps } from './components/Buttons/Button/Button';
 export { IconButton, IconButtonProps } from './components/Buttons/IconButton/IconButton';
 export { Icon, IconProps } from './components/Icon/Icon';
+export {
+  FeedbackCollector,
+  FeedbackCollectorProps,
+  IFeedbackCollector
+} from './components/FeedbackCollector/FeedbackCollector';
+export { ArbolusAvatar, ArbolusAvatarProps } from './components/ArbolusAvatar/ArbolusAvatar';
