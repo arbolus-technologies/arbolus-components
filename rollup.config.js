62d5234e126ca09fedc601a2a9e81fe57047a1fe
Josep Salvat <josep.salvat@arbolus.com>
2023-11-28 14:38:34 +0100

    fix(testing): no

---
+++ rollup.config.js
@@ -0,0 +1,6 @@
+export default {
+  entry: 'src/index.js',
+  dest: 'bundle.js',
+  format: 'cjs',
+  external: ['react-i18next', 'i18next']
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import dts from 'vite-plugin-dts';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import rollupConfig from './rollup.config';
 
 export default defineConfig({
   build: {
@@ -11,15 +12,7 @@ export default defineConfig({
       name: 'arbolus-ui-components',
       fileName: (format) => `index.${format}.js`
     },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    },
+    rollupOptions: rollupConfig,
     sourcemap: true,
     emptyOutDir: true
   },
