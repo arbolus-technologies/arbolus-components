86d3263dc0eb3225e3c0b248b55d010760e7ae7e
Josep Salvat <josep.salvat@arbolus.com>
2023-11-28 09:43:58 +0100

    fix(testing 18n): Fixing i18n

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import App from './App';
 import './index.scss';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import '../i18next.ts';
+import i18n from '../i18next';
+import { I18nextProvider } from 'react-i18next';
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>
+);
